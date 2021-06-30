360c7260cd5a1870b4fd19e2b30960b086521020
Zavala <toro.d.c@gmail.com>
2021-06-30 18:42:38 -0400

    table class

--- app.py
+++ app.py
@@ -6,12 +6,50 @@ from os import environ
 # Create Flask Instance
 app = Flask(__name__)
 # Add Database
-#app.config["SQLALCHEMY_DATABASE_URI"] = envirn.get("DATABASE_URL", "<postgresql connection string>")
+#app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL", "<postgresql+psycopg2://username:password@host:port/database>")
+
 # Initialize the Database
 #db = SQLAlchemy(app)
 
 # Create a Class
-
+#class pokemon(db.Model):
+    # against_bug
+    # against_dark
+    # against_dragon
+    # against_electric
+    # against_fairy
+    # against_fight
+    # against_fire
+    # against_flying
+    # against_ghost
+    # against_grass
+    # against_ground
+    # against_ice
+    # against_normal
+    # against_poison
+    # against_psychic
+    # against_rock
+    # against_steel
+    # against_water
+    # attack
+    # base_egg_steps
+    # base_happiness
+    # base_total
+    # defense
+    # experience_growth
+    # height_m
+    # hp
+    # name
+    # percentage_male
+    # pokedex_number
+    # sp_attack
+    # sp_defense
+    # speed
+    # type1
+    # type2
+    # weight_kg
+    # generation
+    # is_legendary
 
 @app.route("/")
 def index():
